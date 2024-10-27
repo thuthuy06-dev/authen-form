e0841fde3cfeca8ed21aee9a438a891acace98c0
Hieu Quang Truong <hieu@wearebloom.com.au>
2024-10-27 23:26:28 +0700

    FEAT: Implement sign in sign up logic

--- jvv.js
+++ jvv.js
@@ -9,3 +9,27 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+let user = {};
+document.getElementById('signUpForm').addEventListener('submit', (e => SignUp(e)))
+
+document.getElementById('signInForm').addEventListener('submit', (e => SignIn(e)));
+
+function SignUp (event) {
+  event.preventDefault();
+    const username = document.getElementById('signUpUsername').value;
+    const email = document.getElementById('signUpEmail').value;
+    const password = document.getElementById('signUpPassword').value;
+    user = { username, email, password };
+    alert('Sign up successful!');
+}
+
+function SignIn (event) { 
+	event.preventDefault();
+	const email = document.getElementById('signInEmail').value;
+	const password = document.getElementById('signInPassword').value;
+	if (user.email === email && user.password === password) {
+		window.location.href = './landing/index.html';
+	} else {
+		alert('Incorrect email or password!');
+	}
+}
